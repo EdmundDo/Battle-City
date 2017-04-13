5099d4c011f9f86f82808f18b135eb73cd7c545d
Edmund Do <edo@uvm.edu>
2017-04-13 15:59:08 -0400

    Added color struct

---
+++ Battle City/Color.hpp
@@ -0,0 +1,11 @@
+//
+//  Color.cpp
+//  Battle City
+//
+//  Created by Edmund Do on 4/13/17.
+//  Copyright © 2017 Edmund Do. All rights reserved.
+//
+
+struct Color {
+    double red, green, blue;
+};
