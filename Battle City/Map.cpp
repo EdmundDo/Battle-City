61affba2c8cbb57fef4f65b5b42f521c0f252311
Edmund Do <edo@uvm.edu>
2017-04-13 14:48:37 -0400

    Prototype map declaration and implementation

--- Battle City/Map.cpp
+++ Battle City/Map.cpp
@@ -7,3 +7,60 @@
 //
 
 #include "Map.hpp"
+
+Map::Map(int width, int height) : width(width), height(height) {}
+
+Map::~Map() {}
+
+Obstacle* Map::getObstacleAt(int x, int y) {
+    for(int i = 0; i < mapObjs.size(); i++) {
+        if(x == mapObjs[i].getX() && y == mapObjs.getY()) {
+            if(Obstacle o = dynamic_cast<Obstacle*>(mapObjs[i])) {
+                return o;
+            }
+        }
+    }
+    
+    return nullptr;
+}
+
+Terrain* Map::getTerrainAt(int x, int y) {
+    for(int i = 0; i < mapObjs.size(); i++) {
+        if(x == mapObjs[i].getX() && y == mapObjs.getY()) {
+            if(Terrain t = dynamic_cast<Terrain*>(mapObjs[i])) {
+                return t;
+            }
+        }
+    }
+    
+    return nullptr;
+}
+
+void Map::addPreferredStartCoord(int x, int y) {
+    Point2D point = {x, y};
+    preferredStartCoords.push_back(point);
+}
+
+void Map::removePreferredStartCoord(int x, int y) {
+    for(int i = 0; i < preferredStartCoords.size(); i++) {
+        if(preferredStartCoords[i].x == x && preferredStartCoords[i].y == y) {
+            preferredStartCoords.erase(preferredStartCoords.begin() + i);
+        }
+    }
+}
+
+void Map::addMapObj(MapObject &mobj) {
+    mapObjs.push_back(&mobj);
+}
+
+void Map::removeMapObjAt(int x, int y) {
+    for(int i = 0; i < mapObjs.size(); i++) {
+        if(mapObjs[i].getX() == x && mapObjs[i].getY() == y) {
+            mapObjs.erase(mapObjs.begin() + i);
+        }
+    }
+}
+
+vector<Point2D> Map::getPreferredStartCoords() const {
+    return preferredStartCoords;
+}
--- Battle City/Map.hpp
+++ Battle City/Map.hpp
@@ -9,6 +9,34 @@
 #ifndef Map_hpp
 #define Map_hpp
 
-#include <stdio.h>
+#include <vector>
+using namespace std;
+
+struct Point2D {
+    int x;
+    int y;
+};
+
+class Map {
+public:
+    Map(int width, int height);
+    ~Map();
+    
+    MapObject* getObstacleAt(int x, int y);
+    Terrain* getTerrainAt(int x, int y);
+    
+    vector<Point2D> getPreferredStartCoords() const;
+    void addPreferredStartCoord(int x, int y);
+    void removePreferredStartCoord(int x, int y);
+    
+    void addMapObj(MapObject &mobj);
+    void removeMapObjAt(int x, int y);
+    
+private:
+    vector<Point2D> preferredStartCoords;
+    vector<MapObject> mapObjs;
+    int width, height;
+    
+};
 
 #endif /* Map_hpp */
