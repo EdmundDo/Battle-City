27ed192a1f9d408c5cf27f7f7c8c1231d58008ba
Edmund Do <edo@uvm.edu>
2017-04-18 21:32:10 -0400

    Created Point2D classes

---
+++ Battle City/Point2D.cpp
@@ -0,0 +1,9 @@
+//
+//  Point2D.cpp
+//  Battle City
+//
+//  Created by Edmund Do on 4/18/17.
+//  Copyright © 2017 Edmund Do. All rights reserved.
+//
+
+#include "Point2D.hpp"
---
+++ Battle City/Point2D.hpp
@@ -0,0 +1,14 @@
+//
+//  Point2D.hpp
+//  Battle City
+//
+//  Created by Edmund Do on 4/18/17.
+//  Copyright © 2017 Edmund Do. All rights reserved.
+//
+
+#ifndef Point2D_hpp
+#define Point2D_hpp
+
+#include <stdio.h>
+
+#endif /* Point2D_hpp */
