8c5148ae5af21b98d5aeb54933cf40e97c65bd1b
Edmund Do <edo@uvm.edu>
2017-04-18 21:40:03 -0400

    Added RMEs

--- Battle City/Point2D.cpp
+++ Battle City/Point2D.cpp
@@ -7,3 +7,27 @@
 //
 
 #include "Point2D.hpp"
+
+Point2D::Point2D() : Point2D(0, 0) {}
+
+Point2D::Point2D(int x, int y) : x(x), y(y) {}
+
+int Point2D::getX() const {
+    return x;
+}
+
+void Point2D::setX(int x) {
+    this->x = x;
+}
+
+int Point2D::getY() const {
+    return y;
+}
+
+void Point2D::setY(int y) {
+    this->y = y;
+}
+
+double Point2D::distance(const Point2D &p1, const Point2D &p2) {
+    return 0;
+}
--- Battle City/Point2D.hpp
+++ Battle City/Point2D.hpp
@@ -9,6 +9,33 @@
 #ifndef Point2D_hpp
 #define Point2D_hpp
 
-#include <stdio.h>
+class Point2D {
+public:
+    Point2D();
+    Point2D(int x, int y);
+    
+    /**
+     * Requires: nothing
+     * Modifies: nothing
+     * Effects: getters and setters
+     */
+    
+    int getX() const;
+    void setX(int x);
+    int getY() const;
+    void setY(int y);
+    
+    /**
+     * Requires: nothing
+     * Modifies: nothing
+     * Effects: calculates the difference between the two points
+     */
+    
+    static double distance(const Point2D &p1, const Point2D &p2);
+    
+private:
+    int x;
+    int y;
+};
 
 #endif /* Point2D_hpp */
