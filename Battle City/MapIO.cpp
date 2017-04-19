fe04c0efa7a766d946d737646b1c81deca062b5a
Edmund Do <edo@uvm.edu>
2017-04-19 09:48:02 -0400

    Created method declarations for MapIO

---
+++ Battle City/MapIO.cpp
@@ -0,0 +1,9 @@
+//
+//  MapIO.cpp
+//  Battle City
+//
+//  Created by Edmund Do on 4/19/17.
+//  Copyright © 2017 Edmund Do. All rights reserved.
+//
+
+#include "MapIO.hpp"
---
+++ Battle City/MapIO.hpp
@@ -0,0 +1,55 @@
+//
+//  MapIO.hpp
+//  Battle City
+//
+//  Created by Edmund Do on 4/19/17.
+//  Copyright © 2017 Edmund Do. All rights reserved.
+//
+
+#ifndef MapIO_hpp
+#define MapIO_hpp
+
+#include "Map.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class MapIO {
+public:
+    MapIO();
+    ~MapIO();
+    
+    /**
+     * Requires: filepath exists
+     * Modifies: filepath
+     * Effects: getters and setters
+     */
+    
+    void setFilePath(string filepath);
+    void getFilePath() const;
+    
+    /**
+     * Requires: nothing
+     * Modifies: nothing
+     * Effects: writes the mapbojects of the map into a file
+     */
+    
+    void write(Map &map);
+    
+    /**
+     * Requires: filepath exists
+     * Modifies: nothing
+     * Effects: reads the mapobjects of the Map into a new Map
+     */
+    
+    void read();
+    
+private:
+    string filepath;
+    ofstream out;
+    ifstream in;
+};
+
+#endif /* MapIO_hpp */
