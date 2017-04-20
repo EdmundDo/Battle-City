aec4347658296f84394221da3c34877ed0bc555e
Xiao1995329 <xhe2@uvm.edu>
2017-04-19 21:12:27 -0400

    Update MapObject.hpp

--- Battle City/MapObject.hpp
+++ Battle City/MapObject.hpp
@@ -1,14 +1,45 @@
 //
 //  MapObject.hpp
-//  Battle City
+//  CS120
 //
-//  Created by Edmund Do on 4/3/17.
-//  Copyright © 2017 Edmund Do. All rights reserved.
+//  Created by Shaw on 4/13/17.
+//  Copyright © 2017 Shaw. All rights reserved.
 //
 
 #ifndef MapObject_hpp
 #define MapObject_hpp
-
+#include <iostream>
+using namespace std;
 #include <stdio.h>
+#include"Color.hpp"
+
+class MapObject{
+    
+public:
+    MapObject();
+    
+    string getName();
+    
+    int getCoordX();
+    
+    int getCoordY();
+    
+    void draw();
+    
+    
+    
+protected:
+    
+    
+    string name;
+    int coordX;
+    int coordY;
+    
+    Color color;
+    
+};
+
+
+
 
 #endif /* MapObject_hpp */
